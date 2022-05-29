6dad83d73b1939544ff09ce541fc9f77157c49a0
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-05-29 15:50:29 +0300

    implementing java script for adding restaurant to db

---
+++ javascript/addrestaurant.js
@@ -0,0 +1,39 @@
+var title = document.getElementById("addtitle");
+var image = document.getElementById("addimage");
+var desc = document.getElementById("adddescription");
+var s;
+document.getElementById("addrestbtn").addEventListener("click",function2);
+function encodeImageFileAsURL(element) {
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      s=reader.result
+    }
+    reader.readAsDataURL(file);
+  }
+function function2(){ 
+
+    if (title.value=="" ||image.value=="" ||desc.value==""){
+        alert("fill all data please");
+    }
+    else{
+        
+        
+        let data2=new FormData();
+        data2.append("R_name",title.value);
+        data2.append("Desc",desc.value);
+        data2.append("Img",s);
+        
+        axios({
+            method: 'post',
+            url: 'http://localhost/workshopSe/add_restaurant.php',
+            data: data2,
+        })
+        .then(function (response) {
+            console.log(response.data)
+           
+
+        })
+
+    }
+}
--- javascript/admin_userlist.js
+++ javascript/admin_userlist.js
@@ -1,8 +1,8 @@
 axios({ method: 'get', url:'http://localhost/workshopSe/adminuserlist.php',})
 .then(function (response){
     console.log(response.data[0]["F_Name"])
-    document.getElementById("listtable").innerHTML="<table ><tr><th>name</th><th>lastname</th><th>email</th><th>password</th></tr></table>"
+    document.getElementById("listtable").innerHTML=" <tr><th>name</th><th>lastname</th><th>email</th><th>password</th></tr>"
     for (var i=0; i<response.data.length;i++){
-        document.getElementById("listtable").innerHTML +="<table ><tr><td>"+response.data[i]["F_Name"]+"</td><td>"+response.data[i]["L_Name"]+"</td><td>"+response.data[i]["Email"]+"</td><td>"+response.data[i]["Password"]+"</td></tr></table>"; 
+        document.getElementById("listtable").innerHTML +="<tr><td>"+response.data[i]["F_Name"]+"</td><td>"+response.data[i]["L_Name"]+"</td><td>"+response.data[i]["Email"]+"</td><td>"+response.data[i]["Password"]+"</td></tr>"; 
     }
 })
