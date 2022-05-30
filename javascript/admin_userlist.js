83113080c26861dfb53d91e9718dd4fc15961a37
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-05-31 02:05:28 +0300

    changing the design of the login page

--- javascript/admin_userlist.js
+++ javascript/admin_userlist.js
@@ -1,8 +1,18 @@
-axios({ method: 'get', url:'http://localhost/workshopSe/adminuserlist.php',})
-.then(function (response){
-    console.log(response.data[0]["F_Name"])
-    document.getElementById("listtable").innerHTML=" <tr><th>name</th><th>lastname</th><th>email</th><th>password</th></tr>"
-    for (var i=0; i<response.data.length;i++){
-        document.getElementById("listtable").innerHTML +="<tr><td>"+response.data[i]["F_Name"]+"</td><td>"+response.data[i]["L_Name"]+"</td><td>"+response.data[i]["Email"]+"</td><td>"+response.data[i]["Password"]+"</td></tr>"; 
-    }
-})
+axios({
+  method: "get",
+  url: "http://localhost/workshopSe/adminuserlist.php",
+}).then(function (response) {
+  console.log(response.data[0]["F_Name"]);
+  document.getElementById("listtable").innerHTML =
+    " <tr><th>Name</th><th>Lastname</th><th>Email</th></tr>";
+  for (var i = 0; i < response.data.length; i++) {
+    document.getElementById("listtable").innerHTML +=
+      "<tr><td>" +
+      response.data[i]["F_Name"] +
+      "</td><td>" +
+      response.data[i]["L_Name"] +
+      "</td><td>" +
+      response.data[i]["Email"] +
+      "</td></tr>";
+  }
+});
--- javascript/viewrestaurant.js
+++ javascript/viewrestaurant.js
@@ -1,31 +1,32 @@
 axios({
-        method: 'get',
-        url: 'http://localhost/workshopSe/get_restaurant.php',
-    })
-    .then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-            console.log(response.data)
-            var base64img = response.data[i]["Img"];
-            let t= response.data[i]["R_Name"]
-            let d=response.data[i]["Description"]
-            function Base64ToImage(base64img, callback) {
-                var img = new Image();
-                img.onload = function () {
-                    callback(img);
-                };
-                img.addEventListener("click",function(){
-                    location.href="http://127.0.0.1:5500/html/user_review.html"
-                })
-                img.src = base64img;
-            }
-            Base64ToImage(base64img, function (img) {
-                const newDiv = document.createElement("div");
-                newDiv.append(t,img)
-                newDiv.classList.add("restor");
+  method: "get",
+  url: "http://localhost/workshopSe/get_restaurant.php",
+}).then(function (response) {
+  for (var i = 0; i < response.data.length; i++) {
+    console.log(response.data);
+    var base64img = response.data[i]["Img"];
+    let rid = response.data[i]["R_Id"];
+    let t = response.data[i]["R_Name"];
+    let d = response.data[i]["Description"];
 
+    function Base64ToImage(base64img, callback) {
+      var img = new Image();
+      img.onload = function () {
+        callback(img);
+      };
+      img.src = base64img;
+    }
+    Base64ToImage(base64img, function (img) {
+      const newDiv = document.createElement("div");
+      newDiv.id = rid;
+      newDiv.append(t, img, d);
+      newDiv.classList.add("restor");
+      newDiv.addEventListener("click", function () {
+        localStorage.setItem("Img", this.id);
+        location.href = "http://127.0.0.1:5500/html/user_review.html";
+      });
 
-                document.getElementById('addrestaurant').append(newDiv)
-            });
-        }
-
-    })
+      document.getElementById("addrestaurant").append(newDiv);
+    });
+  }
+});
