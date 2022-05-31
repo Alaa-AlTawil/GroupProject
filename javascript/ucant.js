0fcfe578459bcc4d17f3236084f17abc8c812fff
nour az <naz.199860@gmail.com>
2022-05-31 21:42:35 +0300

    removing ucant not working

--- javascript/ucant.js
+++ javascript/ucant.js
@@ -1,9 +1,9 @@
-(function(){
-if (localStorage.user_id !== null) {
-    userfromid(localStorage.user_id, function(data) {
-        if (data.Role != 'admin') {
-            location.href='http://127.0.0.1:5500/GroupProject/index.html'
-        }
-    })
-}
-})();
+// (function(){
+// if (localStorage.user_id !== null) {
+//     userfromid(localStorage.user_id, function(data) {
+//         if (data.Role != 'admin') {
+//             location.href='http://127.0.0.1:5500/GroupProject/index.html'
+//         }
+//     })
+// }
+// })();
